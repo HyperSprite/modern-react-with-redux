5a28d57ff6094c0be5663f1c7469c7e6d3a0995a
HyperSprite <chris-github@hypersprite.com>
2016-02-20 10:56:36 -0800

    added API keys and youtube api search, lecture 13

--- 1-an-intro-to-react/ReduxSimpleStarter/src/index.js
+++ 1-an-intro-to-react/ReduxSimpleStarter/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// the api-keys file is outside of this repo,
+// see api-keys-sample.js in the root for an example
+import ApiKeys from '../../../../api-keys'
+
+const API_KEY = ApiKeys.youtube;
 
 const App = () => {
   return <div>Hi!</div>;
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
---
+++ api-keys-sample.js
@@ -0,0 +1,12 @@
+// If you are using my repo as cloned,
+// place this file one directory up
+// (outside of this folder)
+// and rename it to api-keys.js
+// Then add your own API keys to it
+// See index.js in src directory for more information.
+
+const apiKeys = {
+  youtube: 'put_your_API_Key_here',
+};
+
+module.exports = apiKeys;
